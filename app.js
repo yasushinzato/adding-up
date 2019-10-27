113b3fd65089a213deb7892b46a4905c1e180a4f
yasushinzato <ahzinots.uuiyyska@gmail.com>
2019-10-27 11:22:32 +0000

    ファイルの読み込み処理実装

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+// ファイルを読み込むためのモジュールの呼び出し
+const fs = require('fs');
+const readline = require('readline');
+// 直下のCSVファイルを読み込み。
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {}});
+//[line]イベントが発生したら、1行分の文字[lineString]を表示する。
+rl.on('line', (lineString) => {
+    console.log(lineString);
+});
+rl.resume();
