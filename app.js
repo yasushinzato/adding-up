ec03b4bdd380588b53524cbeb58bd67712cff7ea
yasushinzato <ahzinots.uuiyyska@gmail.com>
2019-10-27 11:30:22 +0000

    2010年と2015年の「集計年」「都道府県 」「15～19歳の人口」を抜き出す実装

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const readline = require('readline');
 // 直下のCSVファイルを読み込み。
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {}});
-//[line]イベントが発生したら、1行分の文字[lineString]を表示する。
+//[line]イベントが発生したら、1行分の文字[lineString]から集計年、都道府県、15～19歳の人工を抜き出す。
 rl.on('line', (lineString) => {
-    console.log(lineString);
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[2];
+    const popu = parseInt(columns[7]);
+    if (year == 2010 || year == 2015) {
+        console.log(year);
+        console.log(prefecture);
+        console.log(popu);
+    }
 });
 rl.resume();
